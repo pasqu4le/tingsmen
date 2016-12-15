14c3d0b6846ceb809e37af69215cce7b824f8bd1
pasqu4le <pasqu4le@gmail.com>
2016-12-15 20:47:24 +0100

    refactoring and basic maling list implemented

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,89 @@
 import os
-import database
-import forms
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, abort, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 # take configuration keys from environment variables:
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # database:
-db = SQLAlchemy(app.application())
-db.create_all()
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def home():
-    return render_template("home.html", title="Welcome")
+    return render_template("index.html", title="Welcome")
 
 
 @app.route('/subscribe/<mailing_list>/', methods=('GET', 'POST'))
 def subscribe(mailing_list):
-    ml = database.MailingList(mailing_list)
-    if not database.MailingList.query.filter_by(name='mailing_list').first():
-        database.db.session.add(ml)
-        return "mailinglist has been added right now"
-    else:
-        return "was alredy there"
+    # basics:
+    title = 'Subscribe'
+    template = 'subscribe.html'
+    # retrieve the mailing list from the database
+    ml = database.MailingList.query.filter_by(name=mailing_list).first()
+    # check if mailing list exists
+    if not ml:
+        abort(404)
+    # check if the form has been correctly filled
     form = forms.EmailForm()
+    errors = []
     if form.validate_on_submit():
-        return render_template("subscribe.html", title="Subscribe", name=mailing_list)
-    return render_template("subscribe.html", title="Subscribe", form=form, name=mailing_list)
+        # if so, add the mail to the mailing list
+        mail = str(form.email.data)
+        em = database.Email.query.filter_by(value=mail).first()
+        # if it's not in the database add it
+        if not em:
+            em = database.Email(mail)
+            db.session.add(em)
+        lnk = database.EmailMailingList.query.filter_by(mailing_list_id=ml.id, email_id=em.value).all()
+        if lnk:
+            errors.append("This email is already registered to this mailing list")
+        else:
+            lnk = database.EmailMailingList(em, ml)
+            db.session.add(lnk)
+            db.session.commit()
+            db.session.flush()
+            return render_template(template, title=title, name=mailing_list)
+    # if not, add the form errors and retry:
+    errors.extend([v for value in form.errors.values() for v in value])
+    return render_template(template, title=title, form=form, name=mailing_list, errors=errors)
+
+
+@app.route('/unsubscribe/<mailing_list>/', methods=('GET', 'POST'))
+def unsubscribe(mailing_list):
+    # basics:
+    title = 'Unsubscribe'
+    template = 'subscribe.html'
+    # retrieve the mailing list from the database
+    ml = database.MailingList.query.filter_by(name=mailing_list).first()
+    # check if mailing list exists
+    if not ml:
+        abort(404)
+    form = forms.EmailForm()
+    # check if the form has been correctly filled
+    errors = []
+    if form.validate_on_submit():
+        em = database.Email.query.filter_by(value=(str(form.email.data))).first()
+        if not em:
+            errors.append("This email is not in the database")
+        else:
+            bridge = database.EmailMailingList.query.filter_by(email_id=em.value, mailing_list_id=ml.id).first()
+            if not bridge:
+                errors.append("This email is already not subscribed to this mailing list")
+            else:
+                db.session.delete(bridge)
+                db.session.commit()
+                db.session.flush()
+                return render_template(template, title=title, name=mailing_list)
+    errors.extend([v for value in form.errors.values() for v in value])
+    return render_template(template, title=title, form=form, name=mailing_list, errors=errors)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+# imported at the end because of dependencies
+from app import database, forms
