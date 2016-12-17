2ea7d25226a4ac25ebd40c535afdf102bb11b039
pasqu4le <pasqu4le@gmail.com>
2016-12-17 20:47:42 +0100

    solved username auth bug

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,7 @@ class EmailForm(FlaskForm):
 
 
 class CustomLoginForm(flask_security.forms.LoginForm):
-    email = StringField('email', validators=[DataRequired(), Email()], render_kw={'placeholder': 'Username or Email Address', 'class': 'form-control'})
+    email = StringField('email', validators=[DataRequired()], render_kw={'placeholder': 'Username or Email Address', 'class': 'form-control'})
     password = PasswordField('password', validators=[DataRequired()], render_kw={'placeholder': 'Password', 'class': 'form-control'})
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login', render_kw={'placeholder': 'Login', 'class': 'btn btn-lg btn-primary btn-block'})
