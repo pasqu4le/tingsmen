6da361e9c9cdf7302da9dbd442a3ff6cfc389c16
pasqu4le <pasqu4le@gmail.com>
2016-12-20 16:36:45 +0100

    render_field macro to render with bootstrap

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,7 @@ class EmailForm(FlaskForm):
 
 
 class CustomLoginForm(flask_security.forms.LoginForm):
-    email = StringField('email', validators=[DataRequired()], render_kw={'placeholder': 'Username or Email Address', 'class': 'form-control'})
-    password = PasswordField('password', validators=[DataRequired()], render_kw={'placeholder': 'Password', 'class': 'form-control'})
-    remember = BooleanField('Remember Me')
+    email = StringField('email', validators=[DataRequired()], render_kw={'placeholder': 'Username or Email Address'})
+    password = PasswordField('password', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
+    remember = BooleanField('Remember Me', render_kw={'class': 'form-inline'})
     submit = SubmitField('Login', render_kw={'placeholder': 'Login', 'class': 'btn btn-lg btn-primary btn-block'})
