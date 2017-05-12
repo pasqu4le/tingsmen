32b3f823792a7be8e4c1ce389e49b601e8becd44
pasqu4le <pasqu4le@gmail.com>
2017-05-12 17:14:02 +0200

    p.38,39 - html email and invites

--- app/cron.py
+++ app/cron.py
@@ -1,6 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
+from flask import render_template
+from flask_mail import Message
+
 
 class CronMail:
 
@@ -18,3 +21,23 @@ class CronMail:
                 with self.mail.connect() as conn:
                     for message in self.messages:
                         conn.send(message)
+
+    def send_notif_message(self, notif):
+        msg = Message('Update on ' + notif.source_type + ' ' + str(notif.source_id), recipients=[notif.user.email])
+        msg.body = notif.to_text() + '\n\nsee it here: ' + notif.link
+        options = {
+            'notif': notif
+        }
+        msg.html = render_template('email/notif.html', **options)
+        self.messages.append(msg)
+
+    def send_invite_message(self, email, sender):
+        msg = Message('Invitation to Tingsmen', recipients=[email])
+        msg.body = sender + ' invited you to join Tingsmen\n\nTingsmen is a social network where you can propose any ' \
+                            'change to the platform and, supposing the community is ok with it, it will be developed' \
+                            '\n\nWanna know more? visit: https://tingsmen.herokuapp.com'
+        options = {
+            'sender': sender
+        }
+        msg.html = render_template('email/invite.html', **options)
+        self.messages.append(msg)
--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 from app.database import LawGroup
 from flask_security.forms import email_required, email_validator, unique_user_email, valid_user_email
-from flask_security.forms import LoginForm, RegisterForm, ForgotPasswordForm, SendConfirmationForm
+from flask_security.forms import LoginForm, RegisterForm, ForgotPasswordForm, SendConfirmationForm, ResetPasswordForm
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import generate_csrf
 from werkzeug.datastructures import MultiDict
 from wtforms import StringField, PasswordField, BooleanField, TextAreaField, HiddenField, FieldList, FormField,\
     SelectMultipleField, RadioField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Email
 from wtforms.widgets.core import html_params
 from wtforms.widgets import HTMLString
 
@@ -32,7 +32,7 @@ class PostForm(FlaskForm):
     parent_id = HiddenField('parent_id')
     content = TextAreaField('content', validators=[DataRequired()], render_kw={'placeholder': 'Content', 'rows': '5',
                                                                                'autocomplete': 'off'})
-    submit = InlineSubmitField('Post', render_kw={'placeholder': 'Post', 'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Post', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
 
     def reset(self):
         # allow to reset this form fields
@@ -44,15 +44,14 @@ class LawForm(FlaskForm):
     content = TextAreaField(render_kw={'placeholder': 'Content', 'rows': '5'})
     groups = SelectMultipleField(choices=[(g.name, g.name) for g in LawGroup.query.all() if g.name != 'Base'],
                                  render_kw={'placeholder': 'Groups', 'class': 'form-control group_form'})
-    submit = InlineSubmitField('Submit',
-                               render_kw={'placeholder': 'Submit', 'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Submit', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
 
 
 class ProposalForm(FlaskForm):
     description = TextAreaField(validators=[DataRequired()], render_kw={'placeholder': 'Description', 'rows': '7'})
     new_laws = FieldList(FormField(LawForm), min_entries=1)
     remove_laws = FieldList(StringField(render_kw={'placeholder': 'LawNumber'}), min_entries=1)
-    submit = InlineSubmitField('Submit', render_kw={'placeholder': 'Post', 'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Submit', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
 
 
 class SettingsForm(FlaskForm):
@@ -66,7 +65,7 @@ class SettingsForm(FlaskForm):
     delete = BooleanField('Delete my account', render_kw={'class': 'form-inline'})
     del_confirm = BooleanField('Confirm deletion', render_kw={'class': 'form-inline'})
     del_posts = BooleanField('Delete my posts as well', render_kw={'class': 'form-inline'})
-    submit = InlineSubmitField('Save', render_kw={'placeholder': 'Post', 'class': 'btn btn-lg btn-primary'})
+    submit = InlineSubmitField('Save', render_kw={'class': 'btn btn-lg btn-primary'})
 
 
 class SearchForm(FlaskForm):
@@ -76,6 +75,12 @@ class SearchForm(FlaskForm):
                         default='all', validators=[DataRequired()], render_kw={'class': 'form-inline'})
 
 
+class InviteForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()], render_kw={'placeholder': 'Email'})
+    sender = StringField('sender', validators=[DataRequired()], render_kw={'placeholder': 'Who are you?'})
+    submit = InlineSubmitField('Send Invitation', render_kw={'class': 'btn btn-lg btn-primary'})
+
+
 # ---------- custom flask-security forms
 
 
@@ -83,7 +88,7 @@ class CustomLoginForm(LoginForm):
     email = StringField('email', validators=[DataRequired()], render_kw={'placeholder': 'Username or Email Address'})
     password = PasswordField('password', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
     remember = BooleanField('Remember Me', render_kw={'class': 'form-inline'})
-    submit = InlineSubmitField('Login', render_kw={'placeholder': 'Login', 'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Login', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
 
 
 class CustomRegisterForm(RegisterForm):
@@ -95,19 +100,24 @@ class CustomRegisterForm(RegisterForm):
     password_confirm = PasswordField('retype_password',
                                      validators=[EqualTo('password', message='Passwords do not match')],
                                      render_kw={'placeholder': 'Retype Password'})
-    submit = InlineSubmitField('Register',
-                               render_kw={'placeholder': 'Register', 'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Register', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
 
 
 class CustomForgotPasswordForm(ForgotPasswordForm):
     email = StringField('email', validators=[email_required, email_validator, valid_user_email],
                         render_kw={'placeholder': 'Email Address'})
-    submit = InlineSubmitField('Recover Password', render_kw={'placeholder': 'Recover Password',
-                                                              'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Recover Password', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
 
 
 class CustomSendConfirmationForm(SendConfirmationForm):
     email = StringField('email', validators=[email_required, email_validator, valid_user_email],
                         render_kw={'placeholder': 'Email Address'})
-    submit = InlineSubmitField('Resend Confirmation Email', render_kw={'placeholder': 'Resend Confirmation Email',
-                                                                       'class': 'btn btn-lg btn-primary btn-block'})
+    submit = InlineSubmitField('Resend Confirmation Email', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
+
+
+class CustomResetPasswordForm(ResetPasswordForm):
+    password = PasswordField('password', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
+    password_confirm = PasswordField('retype_password',
+                                     validators=[EqualTo('password', message='Passwords do not match')],
+                                     render_kw={'placeholder': 'Retype Password'})
+    submit = InlineSubmitField('Reset Password', render_kw={'class': 'btn btn-lg btn-primary btn-block'})
